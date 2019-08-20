39cb7f78c1375d96bca97a9225bcccda32375643
wsw0108 <wsw0108@gmail.com>
2019-08-20 21:41:54 +0800

    fix build errors under Windows

--- build.rs
+++ build.rs
@@ -6,24 +6,39 @@ use std::env;
 
 fn main() {
     // Use system libassimp if it exists
-    if let Ok(..) = pkg_config::Config::new().atleast_version("4.0.0").find("assimp") {
-        return
+    if let Ok(..) = pkg_config::Config::new()
+        .atleast_version("4.0.0")
+        .find("assimp")
+    {
+        return;
     }
 
     // Compile assimp from source
     // Disable unnecessary stuff, it takes long enough to compile already
+    let mut build_zlib_flag = "OFF";
+    if !pkg_config::find_library("zlib").is_ok() {
+        build_zlib_flag = "ON";
+    }
     let dst = Config::new("assimp")
         .define("ASSIMP_BUILD_ASSIMP_TOOLS", "OFF")
         .define("ASSIMP_BUILD_TESTS", "OFF")
         .define("ASSIMP_INSTALL_PDB", "OFF")
+        .define("ASSIMP_BUILD_ZLIB", build_zlib_flag)
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("CMAKE_SUPPRESS_DEVELOPER_WARNINGS", "ON")
         .define("LIBRARY_SUFFIX", "")
         .build();
-    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
+    );
 
     // Link to assimp and its dependencies
-    let debug_postfix = if env::var("DEBUG").unwrap() == "true" { "d" } else { "" };
+    let debug_postfix = if env::var("DEBUG").unwrap() == "true" {
+        "d"
+    } else {
+        ""
+    };
     println!("cargo:rustc-link-lib=static=assimp{}", debug_postfix);
     println!("cargo:rustc-link-lib=static=IrrXML{}", debug_postfix);
     if !pkg_config::find_library("zlib").is_ok() {
