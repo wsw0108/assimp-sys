ad186c8a1114ad18d5f3e20142732261b2da01ad
wsw0108 <wsw0108@gmail.com>
2019-08-22 15:59:16 +0800

    fix tests

--- tests/version.rs
+++ tests/version.rs
@@ -6,5 +6,5 @@ fn check_version() {
     let major = unsafe { aiGetVersionMajor() };
     let minor = unsafe { aiGetVersionMinor() };
     assert_eq!(major, 4);
-    assert_eq!(minor, 0);
+    assert_eq!(minor, 1);
 }
